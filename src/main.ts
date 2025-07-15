85d74b2b0f2a4ac78944562551e8c356cc3195ba
mhong1208 <myhong11a32004@gmail.com>
2025-07-15 14:16:23 +0700

    a

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   await app.listen(port);
+  console.log(`🚀 App is running on: http://localhost:${port}`);
 }
 bootstrap();
